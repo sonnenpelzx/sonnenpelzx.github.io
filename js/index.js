641f671050931a4ad50bfaec49e569d7b8f11491
Tim <sonnenpelzxx@gmail.com>
2024-02-09 22:20:59 +0100

    updated Website

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const plan = document.getElementById("plan");
 const auswahl = document.getElementById("auswahl")
-auswahl.addEventListener("change", showSchicht);
+auswahl.addEventListener("change", newRender);
 const schichtTyp = document.getElementById("schichtTyp")
-schichtTyp.addEventListener("change", showSchicht);
+schichtTyp.addEventListener("change", newRender);
 const dateSelector = document.getElementById("dateSelector");
 dateSelector.addEventListener("change", newRender)
+let tableSize = 7
+const vergleich = document.getElementById("vergleich")
+vergleich.addEventListener("change", newRender)
+const range = document.getElementById("range")
+range.addEventListener("change", rangeChange)
 //adebc
 const schichtAblauf = ['s', 's', 's', '-', 'f', 'f', 'f', 'f', '-', 'n', 'n', 'n', 'n', '-', '-', 'f', 'f', 'f', '-', '-', 'n', 'n', 'n', '-', 's', 's', 's', 's', '-', '-', '-', '-', '-', '-', '-'] 
 const schichtStart= new Date('1/2/2024');
@@ -47,18 +52,45 @@ const schichtOffset = { a: 0,
                         b: 14,
                         c: 28}
 
-function getSchicht(plan, offset){
+function getSchicht(plan, offset, schichtAuswahl){
     const today = (dateSelector.value) ? new Date(dateSelector.value): new Date();
     const diffTime = Math.abs(today - schichtStart);
-    const diffDays = (Math.ceil(diffTime / (1000 * 60 * 60 * 24))  - 1  + schichtOffset[auswahl.value])% schichtAblauf.length;
-    return plan[offset + diffDays]
+    const diffDays = (Math.ceil(diffTime / (1000 * 60 * 60 * 24))  - 1  + schichtOffset[schichtAuswahl])% schichtAblauf.length;
+    return plan[(offset + diffDays)%schichtAblauf.length]
+}
+
+function rangeChange(){
+    tableSize = range.value
+    generateTable()
+}
+
+function vergleichSchichten(){
+    for(let i = -1; i < tableSize; i++){
+        plan.rows[0].cells[i + 2].classList.remove("vergleich");
+        plan.rows[0].cells[i + 2].classList.remove("vergleichZocken");
+        plan.rows[0].cells[i + 2].classList.remove("vergleichTreffen");
+    }
+    if(vergleich.value === 'x'){
+        return;
+    }
+    for(let offset = -1; offset < tableSize; offset++){
+        if(getSchicht(schichtAblauf, offset, vergleich.value) === '-' && getSchicht(schichtAblauf, offset, auswahl.value) === '-' && getSchicht(schichtAblauf, offset -1, auswahl.value) != 'n' && getSchicht(schichtAblauf, offset -1, auswahl.value) != 'n'){
+            plan.rows[0].cells[offset + 2].classList.add("vergleich");
+        }
+        else if((getSchicht(schichtAblauf, offset, vergleich.value) === 'f' && getSchicht(schichtAblauf, offset, auswahl.value) === '-') || (getSchicht(schichtAblauf, offset, vergleich.value) === '-' && getSchicht(schichtAblauf, offset, auswahl.value) === 'f') || (getSchicht(schichtAblauf, offset, vergleich.value) === 's' && getSchicht(schichtAblauf, offset, auswahl.value) === '-') || (getSchicht(schichtAblauf, offset, vergleich.value) === '-' && getSchicht(schichtAblauf, offset, auswahl.value) === 's')){
+            plan.rows[0].cells[offset + 2].classList.add("vergleichZocken");
+        }
+        else if(getSchicht(schichtAblauf, offset, vergleich.value) === '-' || getSchicht(schichtAblauf, offset, auswahl.value) === '-'){
+            plan.rows[0].cells[offset + 2].classList.add("vergleichTreffen");
+        }
+    }
 }
 
 function clearTable(){
     for(let i = 1; i <= 25; i++){
         const row = plan.rows[i]
-        for(let j = 1; j < 9; j++){
-            const cell = row.cells[j]
+        for(let j = 0; j <= tableSize; j++){
+            const cell = row.cells[j + 1]
             cell.style.display = "table-cell"
             cell.rowSpan = "1"
             cell.classList.remove("schicht")
@@ -68,8 +100,8 @@ function clearTable(){
 
 function showSchicht(){
     clearTable()
-    for(let offset = -1; offset < 7; offset++){
-        const schicht = getSchicht(schichtAblauf, offset)
+    for(let offset = -1; offset < tableSize; offset++){
+        const schicht = getSchicht(schichtAblauf, offset, auswahl.value)
         if(schicht === '-'){
             continue
         }
@@ -80,17 +112,13 @@ function showSchicht(){
             plan.rows[begin].cells[offset + 2].classList.add("schicht")
             for(let i = begin + 1; i <= 25; i++){
                 const row = plan.rows[i];
-                console.log(`row: ${i}, cell ${offset + 2}`)
-                console.log(row)
                 row.cells[offset + 2].style.display = 'none';
             }
-            if(offset < 6){
+            if(offset < tableSize - 1){
                 plan.rows[1].cells[offset + 3].rowSpan = `${end}`
                 plan.rows[1].cells[offset + 3].classList.add("schicht")
                 for(let i = 2; i <= end; i++){
                     const row = plan.rows[i];
-                    console.log(`row: ${i}, cell ${offset + 3}`)
-                    console.log(row)
                     row.cells[offset + 2].style.display = 'none';
                 }
             }
@@ -100,8 +128,6 @@ function showSchicht(){
             plan.rows[begin].cells[offset + 2].classList.add("schicht")
             for(let i = begin + 1 ; i <= end; i++){
                 const row = plan.rows[i];
-                console.log(`row: ${i}, cell ${offset + 2}`)
-                console.log(row)
                 row.cells[offset + 2].style.display = 'none';
             }
         }
@@ -109,12 +135,15 @@ function showSchicht(){
 }
 
 function generateTable(){
-    for(let i = -1; i < 7; i++){
+    plan.innerHTML = ""
+    const first = document.createElement("tr")
+    first.appendChild(document.createElement("th"))
+    plan.appendChild(first)
+    for(let i = -1; i < tableSize; i++){
         const head = document.createElement("th");
-        const date = (dateSelector.value) ? new Date(dateSelector.value): new Date();
-        const day = date.getDay()
-        const text = document.createTextNode(`${dayTranslate[(day + i)%7]}`)
-        head.appendChild(text);
+        let date = (dateSelector.value) ? new Date(dateSelector.value): new Date();
+        date.setDate(date.getDate() + i) 
+        head.innerHTML = `${dayTranslate[date.getDay()]} ${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`    
         plan.rows[0].appendChild(head);
         plan.rows[0].cells[i + 2].classList.add("weekday")
     }
@@ -124,20 +153,21 @@ function generateTable(){
         const text = document.createTextNode(`${i}:00`)
         cell.appendChild(text)
         row.appendChild(cell)
-        for(let j = 1; j < 9; j++){
+        for(let j = 0; j <= tableSize; j++){
             cell = document.createElement("td");
             row.appendChild(cell)
         }
         plan.appendChild(row)
     }
+    newRender()
 }
 function newRender(){
-    for(let i = -1; i < 7; i++){
-        const date = (dateSelector.value) ? new Date(dateSelector.value): new Date();
-        const day = date.getDay()
-        plan.rows[0].cells[i + 2].innerText = `${dayTranslate[(day + i)%7]}`;
+    for(let i = -1; i < tableSize; i++){
+        let date = (dateSelector.value) ? new Date(dateSelector.value): new Date();
+        date.setDate(date.getDate() + i) 
+        plan.rows[0].cells[i + 2].innerText = `${dayTranslate[date.getDay()]} ${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`    
     }
     showSchicht();
+    vergleichSchichten();
 }
 generateTable();
-showSchicht();
